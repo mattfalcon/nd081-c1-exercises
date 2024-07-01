33518c5f101b97e028091e502c6b22ae16769d7d
Matt Valladarez <mattvalladarez@gmail.com>
2024-06-30 20:06:06 -0500

    Update config.py

--- L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
+++ L3. Azure Storage/Connecting_to_your_App_to_Storage/config.py
@@ -4,13 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or '[SQL_SERVER_GOES_HERE]'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or '[SQL_DATABASE_GOES_HERE]'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or '[SQL_USER_NAME_GOES_HERE]'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or '[SQL_PASSWORD_GOES_HERE]'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacityproject.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'three'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'MyAdmin_'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Admin$1234'
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or '[BLOB_ACCOUNT_GOES_HERE]'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '[BLOB_STORAGE_KEY_GOES_HERE]'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or '[BLOB_CONTAINER_GOES_HERE]'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'exthree'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'E75kLfwg2i1VE5JjQYTnSYzJHdaEcZJyphlRUr52seJo1y557g0YqPWlfeyxrJgnL5BiLBlfZG20+AStta3L0A=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
